6109c59c9969b15aee6cea858d697849d108f17c
Dev Jadiya <devjadiya15@gmail.com>
2025-11-06 07:39:12 +0000

    Added Initial Files

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -12,7 +12,6 @@ export default function HeroSection() {
         <source src="/video/hero.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="absolute inset-0 bg-black/50" />
       <div className="relative z-10 flex h-full flex-col items-center justify-center text-center text-white">
           {/* Content removed as the video is the main focus */}
       </div>
